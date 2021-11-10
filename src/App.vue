a241da307c91a97dc2ce6888d7d24a87f89fcc39
poorpool <489679970@qq.com>
2021-11-10 15:02:27 +0800

    feat(frontend): 基本crud完毕

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,58 @@
           <Input v-model="baseinfo.backendpassword" placeholder="密码" type="password"></Input>
         </FormItem>
       </Form>
-
     </Modal>
+
+    <Modal
+        title="增加新规则"
+        v-model="isAddRule"
+        @on-ok="addRule">
+      <Form :model="baseinfo">
+        <FormItem label="aid">
+          <Input v-model="submitData.aid" type="number"></Input>
+        </FormItem>
+        <FormItem label="platform">
+          <Input v-model="submitData.platform" type="text"></Input>
+        </FormItem>
+        <FormItem label="download_url">
+          <Input v-model="submitData.download_url" type="text"></Input>
+        </FormItem>
+        <FormItem label="update_version_code">
+          <Input v-model="submitData.update_version_code" type="text"></Input>
+        </FormItem>
+        <FormItem label="md5">
+          <Input v-model="submitData.md5" type="md5"></Input>
+        </FormItem>
+        <FormItem label="device_id_list">
+          <Input v-model="submitData.device_id_list" type="text"></Input>
+        </FormItem>
+        <FormItem label="max_update_version_code">
+          <Input v-model="submitData.max_update_version_code" type="text"></Input>
+        </FormItem>
+        <FormItem label="min_update_version_code">
+          <Input v-model="submitData.min_update_version_code" type="text"></Input>
+        </FormItem>
+        <FormItem label="max_os_api">
+          <Input v-model="submitData.max_os_api" type="number"></Input>
+        </FormItem>
+        <FormItem label="min_os_api">
+          <Input v-model="submitData.min_os_api" type="number"></Input>
+        </FormItem>
+        <FormItem label="cpu_arch">
+          <Input v-model="submitData.cpu_arch" type="md5"></Input>
+        </FormItem>
+        <FormItem label="channel">
+          <Input v-model="submitData.channel" type="text"></Input>
+        </FormItem>
+        <FormItem label="title">
+          <Input v-model="submitData.title" type="text"></Input>
+        </FormItem>
+        <FormItem label="update_tips">
+          <Input v-model="submitData.update_tips" type="text"></Input>
+        </FormItem>
+      </Form>
+    </Modal>
+
 
     <div class="layout">
       <Layout>
@@ -46,10 +96,19 @@
             <p slot="title">
               规则展示
             </p>
-            <a href="#" slot="extra" @click.prevent="addRule">
+            <a href="#" slot="extra" @click.prevent="isAddRule=true">
               加入规则
             </a>
-            <Table stripe border :columns="ruleColumns" :data="rules"/>
+            <Table stripe border :columns="ruleColumns" :data="rules">
+              <template slot-scope="{ row, index }" slot="action">
+                <div v-if="rules[index].status >= 0 && rules[index].status <= 1">
+                  <Button type="primary" size="small" style="margin-right: 5px" @click="switchStatus(index)">
+                    切换可用状态
+                  </Button>
+                  <Button type="error" size="small" @click="removeStatus(index)">彻底下线</Button>
+                </div>
+              </template>
+            </Table>
             <Page show-sizer show-elevator show-total :style="{padding: '20px 0'}"
                   :total="page.total" :current="page.index" :page-size="page.size"
                   @on-change="pageIndexChange" @on-page-size-change="pageSizeChange"/>
@@ -70,6 +129,24 @@ export default {
   },
   data () {
     return {
+      isAddRule: false,
+      submitData: {
+        aid: 0,
+        platform: '',
+        download_url: '',
+        update_version_code: '',
+        md5: '',
+        device_id_list: '',
+        max_update_version_code: '',
+        min_update_version_code: '',
+        max_os_api: 0,
+        min_os_api: 0,
+        cpu_arch: '',
+        channel: '',
+        title: '',
+        update_tips: '',
+        status: 0
+      },
       modalbaseinfo: true,
       baseinfo: {
         backendurl: '',
@@ -86,6 +163,11 @@ export default {
         {
           title: "id",
           key: "id",
+          fixed: "left"
+        },
+        {
+          title: "当前状态",
+          key: "showStatus",
           fixed: "left"
         },
         {
@@ -147,6 +229,12 @@ export default {
         {
           title: "status",
           key: "status"
+        },
+        {
+          title: '操作',
+          slot: 'action',
+          width: 150,
+          fixed: "right"
         }
       ]
     }
@@ -154,6 +242,15 @@ export default {
   methods: {
     showBaseInfo() {
       this.modalbaseinfo = true;
+    },
+    status2description(i) {
+      if (i === 0) {
+        return "正常";
+      } else if (i === 1) {
+        return "暂停";
+      } else {
+        return "下线";
+      }
     },
     fetchRules() {
       if (this.baseinfo.backendurl == null || this.baseinfo.backendurl === '') {
@@ -170,6 +267,9 @@ export default {
       }).then(res => {
         if (res.data.code === 200) {
           _this.rules = res.data.data.rules
+          for (let i = 0; i < this.rules.length; i++) {
+            this.rules[i].showStatus = this.status2description(this.rules[i].status);
+          }
         } else {
           this.$Modal.error({
             title: res.data.data.code,
@@ -211,6 +311,110 @@ export default {
           this.$Modal.error({
             title: res.data.data.code,
             content: "拉取规则条数失败"
+          });
+        }
+      }).catch(error => {
+        this.$Modal.error({
+          title: "错误",
+          content: error
+        });
+      });
+    },
+    switchStatus(index) {
+      if (this.rules[index].status < 0 || this.rules[index].status > 1) {
+        return ;
+      }
+      this.$Modal.confirm({
+        title: '确认切换可用状态',
+        content: '<p>你确定要将 id 为 ' + this.rules[index].id + ' 的数据的状态从 '
+            + this.status2description(this.rules[index].status)
+            + " 变为 "
+            + this.status2description(1 - this.rules[index].status)
+            + ' 吗？</p>',
+        onOk: () => {
+          let _this = this;
+          this.axios({
+            method: 'post',
+            url: _this.baseinfo.backendurl + "/configure_rule/update_ruleStatus",
+            params: {
+              id : _this.rules[index].id,
+              toStatus: 1 - _this.rules[index].status
+            }
+          }).then(res => {
+            if (res.data.code === 200) {
+              _this.fetchRules();
+            } else {
+              this.$Modal.error({
+                title: res.data.data.code,
+                content: "切换可用状态失败"
+              });
+            }
+          }).catch(error => {
+            this.$Modal.error({
+              title: "错误",
+              content: error
+            });
+          });
+        },
+        onCancel: () => {
+          this.$Message.info('取消切换可用状态');
+        }
+      });
+    },
+    removeStatus(index) {
+      if (this.rules[index].status < 0 || this.rules[index].status > 1) {
+        return ;
+      }
+      this.$Modal.confirm({
+        title: '确认彻底下线',
+        content: '<p>你确定要将 id 为 ' + this.rules[index].id + ' 的数据的状态从'
+            + this.status2description(this.rules[index].status)
+            + " 变为 "
+            + this.status2description(1 - this.rules[index].status)
+            + ' 吗？</p>',
+        onOk: () => {
+          let _this = this;
+          this.axios({
+            method: 'post',
+            url: _this.baseinfo.backendurl + "/configure_rule/update_ruleStatus",
+            params: {
+              id : _this.rules[index].id,
+              toStatus: 2
+            }
+          }).then(res => {
+            if (res.data.code === 200) {
+              _this.fetchRules();
+            } else {
+              this.$Modal.error({
+                title: res.data.data.code,
+                content: "切换可用状态失败"
+              });
+            }
+          }).catch(error => {
+            this.$Modal.error({
+              title: "错误",
+              content: error
+            });
+          });
+        },
+        onCancel: () => {
+          this.$Message.info('取消彻底下线');
+        }
+      });
+    },
+    addRule() {
+      let _this = this;
+      this.axios({
+        method: 'post',
+        url: _this.baseinfo.backendurl + "/configure_rule",
+        data: _this.submitData
+      }).then(res => {
+        if (res.data.code === 200) {
+          _this.fetchRules();
+        } else {
+          this.$Modal.error({
+            title: "错误 " + res.data.code,
+            content: res.data.message
           });
         }
       }).catch(error => {
